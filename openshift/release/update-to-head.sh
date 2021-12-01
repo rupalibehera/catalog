6a6175459ede3f4e744adf8a882787c17e497277
Rupali Behera <rbehera@redhat.com>
2021-12-01 16:07:51 +0100

    Some more improvements with update-to-head.sh which includes 1. disabling the push to release-next-ci branch as we do trigger any CI after pushing 2. Disabling the PR creation as it is of no use 3. renaming the task name from create-pr to sync-upstream-to-midstream

--- openshift/release/update-to-head.sh
+++ openshift/release/update-to-head.sh
@@ -20,23 +20,3 @@ git add openshift OWNERS_ALIASES OWNERS
 git commit -m ":open_file_folder: Update openshift specific files."
 
 git push -f ${OPENSHIFT_REMOTE} HEAD:release-next
-
-# Trigger CI
-git checkout release-next --no-track -B release-next-ci
-date > ci
-git add ci
-git commit -m ":robot: Triggering CI on branch 'release-next' after synching to upstream/main"
-git push -f ${OPENSHIFT_REMOTE} HEAD:release-next-ci
-
-# removing upstream remote so that hub points origin for hub pr list command due to this issue https://github.com/github/hub/issues/1973
-git remote remove upstream
-
-already_open_github_issue_id=$(hub pr list -s open -f "%I %l%n"|grep ${LABEL}| awk '{print $1}'|head -1)
-[[ -n ${already_open_github_issue_id} ]]  && {
-    echo "PR for nightly is already open on #${already_open_github_issue_id}"
-    #hub api repos/${OPENSHIFT_ORG}/${REPO_NAME}/issues/${already_open_github_issue_id}/comments -f body='/retest'
-    exit 0
-}
-
-hub pull-request -m "🛑🔥 Triggering Nightly CI for ${REPO_NAME} 🔥🛑" -m "/hold" -m "Nightly CI do not merge :stop_sign:" \
-    --no-edit -l "${LABEL}" -b ${OPENSHIFT_REMOTE}/${REPO_NAME}:release-next -h ${OPENSHIFT_REMOTE}/${REPO_NAME}:release-next-ci
